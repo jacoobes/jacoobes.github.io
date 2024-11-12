b21a4d21565545187fe0abb7291367829b60e0f3
Jacob Nguyen <76754747+jacoobes@users.noreply.github.com>
2024-11-12 14:36:50 -0600

    progress

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,16 +1,18 @@
 import fs from 'fs'
 const projects = JSON.parse(fs.readFileSync('static/stuff.json', 'utf8'));
+const links = JSON.parse(fs.readFileSync('static/links.json', 'utf8'));
 
 export default function(eleventyConfig) {
-  console.log(eleventyConfig)
   eleventyConfig.setNunjucksEnvironmentOptions({
         throwOnUndefined: true,
         autoescape: false, // warning: don’t do this!
   });
+  eleventyConfig.setInputDirectory('src');
   eleventyConfig.setOutputDirectory("dist");
   eleventyConfig.addNunjucksGlobal("projects", projects);
-  eleventyConfig.addPassthroughCopy("style.css");
-  eleventyConfig.addPassthroughCopy("index.js");
-  eleventyConfig.addPassthroughCopy("github-mark.svg");
+  eleventyConfig.addNunjucksGlobal("links", links);
+  eleventyConfig.addNunjucksGlobal("quote", "Simplicity is the ultimate sophistication");
+  eleventyConfig.addPassthroughCopy("src/index.js");
+  eleventyConfig.addPassthroughCopy("src/style.css");
 };
 
